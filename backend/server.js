e9e3744ef62c333062da7aa1715d0354dd7be4ea
Shubham-Gedam <shubhgedam28@gmail.com>
2025-08-17 10:00:34 +0530

    feat: add Socket.IO support and integrate with the server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
+const initSocketServer = require('./src/socket/socket.server')
+const httpServer = require('http').createServer(app);
+
 
 connectDB();
+initSocketServer(httpServer);~
 
 
-app.listen(3000, () => {
+httpServer.listen(3000, () => {
   console.log(`Server is running on port 3000`);
 });
